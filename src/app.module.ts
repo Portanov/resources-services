149ce36a2a5f6a406b3bbf6383cda262306083d7
Portanov <natsukitheracoon@gmail.com>
2026-04-17 22:12:34 -0600

    commit_2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClinicalFilterModule } from './services/clinical-filter/clinical-filter.module';
 import { NutricionalCalculatorModule } from './services/nutricional-calculator/nutricional-calculator.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NutricionalCalculatorModule, ClinicalFilterModule],
+  imports: [
+    NutricionalCalculatorModule,
+    ClinicalFilterModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.database_url,
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
